a03893f871a3b5fe07138f3ca104a383bd085237
KorNelia2211 <kroszkowska5@st.swps.edu.pl>
2025-06-16 10:48:42 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+let rh = 60
+let rw = 350
+let s = 25
+
+function preload() {
+  img1 = loadImage('IMG_1300.png');
+  img2 = loadImage('IMG_1301.png');
+  img3 = loadImage('IMG_1302.png');
+  img4 = loadImage('IMG_1303.png');
+}
+
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  rectMode(CENTER);
+  textAlign(CENTER,CENTER);
+  textSize(20);
+  imageMode(CENTER);
+}
+
+function draw() {
+  background('#E9E8E7');
+  
+  let centerX = windowWidth/2;
+  let centerY = windowHeight/2;
+  
+  fill('#FFFFFF');
+  stroke('#8D8D8D');
+  strokeWeight(2);
+  rect(centerX,centerY,rw,rh);
+  
+  
+  noStroke();
+   fill(0);
+  text('Nie jestem robotem',centerX - 75,centerY);
+  
+ if(mouseX > centerX + 150 - s/2 && mouseX < centerX + 150 + s/2 &&
+mouseY > centerY - s/2 && mouseY < centerY + s/2 && mouseIsPressed){
+    fill('#8D8D8D');
+  stroke('#8D8D8D');
+  strokeWeight(2);
+  square(centerX + 150,centerY,s);
+  }else{
+    fill('#FFFFFF');
+  stroke('#8D8D8D');
+  strokeWeight(2);
+  square(centerX + 150,centerY,s);
+    }
+  
+  if(mouseIsPressed){
+    window.location.href = 
+      print('go')
+  }
+    
+  let currentFrame = Math.floor((frameCount % 60) / 15);
+
+if (currentFrame === 0) {
+  image(img1, centerX, centerY - 150,150,234);
+} else if (currentFrame === 1) {
+  image(img2, centerX, centerY - 140,150,234);
+} else if (currentFrame === 2) {
+  image(img3, centerX, centerY - 150,150,234);
+} else {
+  image(img4, centerX, centerY - 140,150,234);
+}
+}
